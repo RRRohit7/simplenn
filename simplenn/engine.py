90dfb7acfa12fa33884bc3c83ea47c304622cae0
Rohit <rohit7rrr@gmail.com>
2025-09-06 21:03:25 -0500

    Added backpropagation via SGD

--- simplenn/engine.py
+++ simplenn/engine.py
@@ -1,29 +1,37 @@
 class Data:
     def __init__(self, value, children=(), op=''):
         self.value = value
+        self.children = children
+        self.op = op
+        self.backward = lambda: None
+        self.grad = 0.0
+
     def __add__(self, other):
-        if isinstance(other, (int, float)):
-            return Data(self.value + other, (self, Data(other), '+'))
-        return Data(self.value + other.value, (self, other), '+')
+        other = other if isinstance(other, Data) else Data(other, op='const')   
+        output = Data(self.value + other.value, (self, other), '+')
+        def backward():
+            self.grad = output.grad
+            other.grad = output.grad
+        output.backward = backward
+        return output   
     def __radd__(self, other):
         return self + other
     def __mul__(self, other):
-        if isinstance(other, (int, float)):
-            return Data(self.value * other, (self, Data(other), '*'))
-        return Data(self.value * other.value, (self, other), '*')
+        other = other if isinstance(other, Data) else Data(other, op='const')
+        output = Data(self.value * other.value, (self, other), '*')
+        def backward():
+            self.grad = other.value * output.grad
+            other.grad = self.value * output.grad
+        output.backward = backward
+        return output
     def __rmul__(self, other):
         return self * other    
-    def __truediv__(self, other):
-        if isinstance(other, (int, float)):
-            return Data(self.value / other, (self, Data(other), '/'))
-        return Data(self.value / other.value, (self, other), '/')
-    def __rtruediv__(self, other):
-        return Data(other / self.value, (other, self), '/')
-    def __mod__(self, other):
-        if isinstance(other, (int, float)):
-            return Data(self.value % other, (self, Data(other), '%'))
-        return Data(self.value % other.value, (self, other), '%')
-    def __rmod__(self, other):
-        return Data(other % self.value, (other, self), '%')
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        output = Data(self.value ** other, (self,), '**')
+        def backward():
+            self.grad = other * (self.value ** (other - 1)) * output.grad
+        output.backward = backward
+        return output
     def __str__(self):
-        return f"Data: {self.value}"
+        return f"Data: {self.value}, operation: {self.op}"
