e4f2b90210f850a52d2f5eaa2d73b624a78a32d7
Rohit <rohit7rrr@gmail.com>
2025-09-06 19:51:51 -0500

    Modified the typecheck for other data object in the engine to better handle edge cases

--- simplenn/engine.py
+++ simplenn/engine.py
@@ -1,29 +1,29 @@
 class Data:
-    def __init__(self, value):
+    def __init__(self, value, children=(), op=''):
         self.value = value
     def __add__(self, other):
         if isinstance(other, (int, float)):
-            return Data(self.value + other)
-        return Data(self.value + other.value)
+            return Data(self.value + other, (self, Data(other), '+'))
+        return Data(self.value + other.value, (self, other), '+')
     def __radd__(self, other):
         return self + other
     def __mul__(self, other):
         if isinstance(other, (int, float)):
-            return Data(self.value * other)
-        return Data(self.value * other.value)
+            return Data(self.value * other, (self, Data(other), '*'))
+        return Data(self.value * other.value, (self, other), '*')
     def __rmul__(self, other):
         return self * other    
     def __truediv__(self, other):
         if isinstance(other, (int, float)):
-            return Data(self.value / other)
-        return Data(self.value / other.value)
+            return Data(self.value / other, (self, Data(other), '/'))
+        return Data(self.value / other.value, (self, other), '/')
     def __rtruediv__(self, other):
-        return Data(other / self.value)
+        return Data(other / self.value, (other, self), '/')
     def __mod__(self, other):
         if isinstance(other, (int, float)):
-            return Data(self.value % other)
-        return Data(self.value % other.value)
+            return Data(self.value % other, (self, Data(other), '%'))
+        return Data(self.value % other.value, (self, other), '%')
     def __rmod__(self, other):
-        return Data(other % self.value)
+        return Data(other % self.value, (other, self), '%')
     def __str__(self):
         return f"Data: {self.value}"
