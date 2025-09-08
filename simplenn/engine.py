f73da1fccc0978eeb0180af63d6260def986e8fc
Rohit <rohit7rrr@gmail.com>
2025-09-07 21:17:30 -0500

    Adding division func to Data and MSE loss function

--- simplenn/engine.py
+++ simplenn/engine.py
@@ -1,4 +1,6 @@
 import math
+from enum import Enum
+
 class Data:
     def __init__(self, value, children=(), op=''):
         self.value = value
@@ -29,6 +31,13 @@ class Data:
             self.grad = (output.value > 0) * output.grad
         output.backward = backward
         return output
+    def sigmoid(self):
+        x = self.value
+        sigmoid = Data(1 / ( 1 + math.exp(x * -1)), (self,), 'Sigmoid')
+        def backward():
+             self.grad = sigmoid.value * (1 - sigmoid.value) * sigmoid.grad
+        sigmoid.backward = backward
+        return sigmoid
     def __mul__(self, other):
         other = other if isinstance(other, Data) else Data(other, op='const')
         output = Data(self.value * other.value, (self, other), '*')
@@ -42,12 +51,16 @@ class Data:
     def __pow__(self, other):
         assert isinstance(other, (int, float))
         other = Data(other, op='const')
-        output = Data(self.value ** other.value, (self, other), '**')
+        output = Data(self.value ** other.value, (self,), '**')
         def backward():
             self.grad = other.value * (self.value ** (other.value - 1)) * output.grad
-            other.grad = math.log(self.value) * (self.value ** other.value) * output.grad
+            # other.grad = math.log(self.value) * (self.value ** other.value) * output.grad
         output.backward = backward
         return output
+    def __truediv__(self, other):
+        return self * other**-1
+    def __rtruediv__(self, other):
+        return other * self**-1 
     def backpropagation(self):
         order = []
         visited = set()
@@ -64,3 +77,18 @@ class Data:
     
     def __str__(self):
         return f"Data: {self.value}, operation: {self.op}"
+
+class LossType(Enum):
+    MSE = 1 # only implementing Mean Square Error as of now
+
+class Loss:
+    def __init__(self, type: LossType):
+        self.type = type
+    
+    def __call__(self, expected, original):
+        assert self.type == LossType.MSE, "Only Supporting Mean Square Error loss for now"
+        loss = sum([(e - o)**2 for e, o in zip(expected, original)]) / len(original)
+        return loss
+        
+
+
